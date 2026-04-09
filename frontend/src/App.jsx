90ea92d6584c48e80ef0d8c4cbbee90e95a8bb0e
JoelLawrence2005 <Joellawrence742005@gmail.com>
2026-04-09 12:31:05 +0530

    Lesson Notes
    Learning React, learning how to navigate components and props

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import Home from './pages/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}/>
-      </Routes>
-    </BrowserRouter>
+    <div className='App'>
+      <h1>Hello World</h1>
+    </div>
   );
 }
 
-export default App
+export default App;
---
+++ notes.md
@@ -0,0 +1,4 @@
+##  Notes
+**19.03.26 21.34**
+
+It's not going great, no progress at all, I need to do something about this, learning curve is shit and it sucks.
