43e3539cc80c82997f3eacddab9d8db556d0b8e4
JoelLawrence2005 <Joellawrence742005@gmail.com>
2026-05-16 13:18:49 +0530

    Change
    I don't know what changed

---
+++ frontend/src/pages/SignUp.jsx
@@ -0,0 +1,12 @@
+function SignUp() {
+    return (
+        <>
+            <form class='form-section'>
+                <label name='sign-up' id='sign-up'>Sign In:</label>
+                <input type='email' />
+                <label name='pswd' id='pswd'>Password</label>
+                <input type='pswd' />
+            </form>
+        </>
+    );
+}
