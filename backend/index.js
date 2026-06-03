acce6650d9b80873e3108b6ebccfe99c12fc177d
Emerald Isle Manpower and Travel Services <admin@emeraldisle.lk>
2026-06-03 22:36:32 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,12 @@ import express from "express"
 import mysql from "mysql2"
 import dotenv from "dotenv"
 import cors from "cors"
-dotenv.config()
+import path from "path"
+import { fileURLToPath } from "url"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+dotenv.config({ path: path.resolve(__dirname, ".env") })
 
 // this is used to load the express web thingy
 const app = express()
